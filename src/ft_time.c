d9d94374df9f6d098f6a5081a5efcb466f3579e4
Diegosou4 <diegoaguia31@gmail.com>
2024-05-01 10:58:57 +0100

    (add) fuctions for time

---
+++ src/ft_time.c
@@ -0,0 +1,28 @@
+
+
+#include "../includes/philo.h"
+
+
+
+int ft_usleep(size_t milliseconds)
+{
+    size_t start;
+
+    start = get_curr_time();
+
+    while((get_curr_time() - start) < milliseconds)
+        usleep(500);
+    return(0);
+}
+
+
+size_t get_curr_time(void)
+{
+    struct timeval time;
+    
+    if(gettimeofday(&time,NULL) == -1)
+    {
+        write(2, "Error in get_curr_time\n",24);
+    }
+    return(time.tv_sec * 1000 + time.tv_usec / 1000);
+}
